7c80bf87ece102c44d0f76e941ad155326278b39
unknown <mdanuwar96@gmail.com>
2024-04-10 14:48:14 +0600

    about page done

---
+++ src/app/about/page.jsx
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react';
+
+const page = () => {
+    return (
+        <Fragment>
+            <div className='max-w-6xl mx-auto p-3 space-y-4'>
+                <h1 className='uppercase text-2xl'>About us</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis ab alias rem, pariatur porro voluptas illum? Et laudantium, maiores corporis accusamus hic optio aut, totam doloremque quidem in, odio nam asperiores tempore sed tempora consequuntur obcaecati! Ut odit ex neque, aliquid, quis animi itaque illo cumque quas alias sapiente explicabo?</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis ab alias rem, pariatur porro voluptas illum? Et laudantium, maiores corporis accusamus hic optio aut, totam doloremque quidem in, odio nam asperiores tempore sed tempora consequuntur obcaecati! Ut odit ex neque, aliquid, quis animi itaque illo cumque quas alias sapiente explicabo?</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis ab alias rem, pariatur porro voluptas illum? Et laudantium, maiores corporis accusamus hic optio aut, totam doloremque quidem in, odio nam asperiores tempore sed tempora consequuntur obcaecati! Ut odit ex neque, aliquid, quis animi itaque illo cumque quas alias sapiente explicabo?</p>
+            </div>
+        </Fragment>
+    );
+};
+
+export default page;
